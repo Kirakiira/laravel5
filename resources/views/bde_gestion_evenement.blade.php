e963e43d5880fcaae6d5272b2d645b5797889195
VincentVexia <vallet.vincent13@gmail.com>
2018-04-11 13:55:47 +0200

    added all pages

--- resources/views/bde_gestion_evenement.blade.php
+++ resources/views/bde_gestion_evenement.blade.php
@@ -5,15 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <link href="resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
-    <link href="data/styles.css" type="text/css" rel="stylesheet"/>
-    <link href="files/bde_-_gestion_evenement/styles.css" type="text/css" rel="stylesheet"/>
-    <script src="resources/scripts/jquery-1.7.1.min.js"></script>
-    <script src="resources/scripts/jquery-ui-1.8.10.custom.min.js"></script>
-    <script src="resources/scripts/prototypePre.js"></script>
-    <script src="data/document.js"></script>
-    <script src="resources/scripts/prototypePost.js"></script>
-    <script src="files/bde_-_gestion_evenement/data.js"></script>
+    <link href="/assets/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
+    <link href="/assets/css/styles.css" type="text/css" rel="stylesheet"/>
+    <link href="/assets/css/bde_gestion_evenement.css" type="text/css" rel="stylesheet"/>
+    <script src="/js/jquery-1.7.1.min.js"></script>
+    <script src="/js/jquery-ui-1.8.10.custom.min.js"></script>
+    <script src="/js/prototypePre.js"></script>
+    <script src="/js/document.js"></script>
+    <script src="/js/prototypePost.js"></script>
+    <script src="/js/bde_gestion_evenement.js"></script>
     <script type="text/javascript">
         $axure.utils.getTransparentGifPath = function() { return 'resources/images/transparent.gif'; };
         $axure.utils.getOtherPath = function() { return 'resources/Other.html'; };
@@ -51,7 +51,7 @@
 
     <!-- Unnamed (Image) -->
     <div id="u204" class="ax_default image">
-        <img id="u204_img" class="img " src="images/accueil/u1.jpg"/>
+        <img id="u204_img" class="img " src="/assets/image/u1.jpg"/>
     </div>
 
     <!-- Unnamed (Rectangle) -->
@@ -64,32 +64,32 @@
 
     <!-- Unnamed (Shape) -->
     <div id="u206" class="ax_default box_1">
-        <img id="u206_img" class="img " src="images/bde_-_gestion_evenement/u206.png"/>
+        <img id="u206_img" class="img " src="/assets/image/u206.png"/>
     </div>
 
     <!-- Unnamed (Shape) -->
     <div id="u207" class="ax_default box_1">
-        <img id="u207_img" class="img " src="images/bde_-_gestion_evenement/u206.png"/>
+        <img id="u207_img" class="img " src="/assets/image/u206.png"/>
     </div>
 
     <!-- Unnamed (Shape) -->
     <div id="u208" class="ax_default box_1">
-        <img id="u208_img" class="img " src="images/bde_-_gestion_evenement/u206.png"/>
+        <img id="u208_img" class="img " src="/assets/image/u206.png"/>
     </div>
 
     <!-- Unnamed (Shape) -->
     <div id="u209" class="ax_default icon">
-        <img id="u209_img" class="img " src="images/bde_-_gestion_evenement/u209.png"/>
+        <img id="u209_img" class="img " src="/assets/image/u209.png"/>
     </div>
 
     <!-- Unnamed (Shape) -->
     <div id="u210" class="ax_default icon">
-        <img id="u210_img" class="img " src="images/bde_-_gestion_evenement/u210.png"/>
+        <img id="u210_img" class="img " src="/assets/image/u210.png"/>
     </div>
 
     <!-- Unnamed (Shape) -->
     <div id="u211" class="ax_default icon">
-        <img id="u211_img" class="img " src="images/bde_-_gestion_evenement/u211.png"/>
+        <img id="u211_img" class="img " src="/assets/image/u211.png"/>
     </div>
 
     <!-- Unnamed (Rectangle) -->
